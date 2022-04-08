27ee0fb078aae563f861838e7db6cf8d79795631
Philippe Gagnon <12717218+pgagnon@users.noreply.github.com>
2022-04-08 09:51:23 -0400

    Add --model option

--- main.py
+++ main.py
@@ -12,51 +12,54 @@ import click
     help="Perform a live or offline migration",
     default="live",
 )
-def migrate_model(source: str, destination: str, mode: str):
-    from airflow.models import (
-        Connection,
-        DagRun,
-        Pool,
-        RenderedTaskInstanceFields,
-        SensorInstance,
-        SlaMiss,
-        TaskFail,
-        TaskInstance,
-        TaskReschedule,
-        Trigger,
-        Variable,
-    )
+@click.option(
+    "--model",
+    multiple=True,
+    default=[
+        "Connection",
+        "DagRun",
+        "Pool",
+        "RenderedTaskInstanceFields",
+        "SensorInstance",
+        "SlaMiss",
+        "TaskFail",
+        "TaskInstance",
+        "TaskReschedule",
+        "Trigger",
+        "Variable",
+    ],
+)
+def migrate_model(source: str, destination: str, mode: str, model: str):
+    import airflow.models as airflow_models
 
     from sqlalchemy.orm import sessionmaker
     from sqlalchemy import create_engine
 
     Source_Session = sessionmaker(bind=create_engine(source))
 
-    for model in [
-        Connection,
-        DagRun,
-        Pool,
-        RenderedTaskInstanceFields,
-        SensorInstance,
-        SlaMiss,
-        TaskFail,
-        TaskInstance,
-        TaskReschedule,
-        Trigger,
-        Variable,
-    ]:
-        print(f"Extracting {model}")
+    for model_ in model:
+        print(f"Querying {model_}")
 
         src_session = Source_Session()
-        objs = src_session.query(model).all()
+        objs = src_session.query(getattr(airflow_models, model_)).all()
 
         if mode == "live":
+            print(f"Merging {model_}")
             Dest_Session = sessionmaker(bind=create_engine(destination))
             dst_session = Dest_Session()
 
             for obj in objs:
                 dst_session.merge(obj)
                 dst_session.commit()
+
+            print(f"Done merging {model_} ({len(objs)} objects)")
+        elif mode == "offline":
+            import pickle, pathlib
+
+            print(f"Saving {model_}")
+            with pathlib.Path(f"{model_}.pickle").open("w+b") as fd:
+                fd.write(pickle.dumps(objs))
+            print(f"Saved {model_} ({len(objs)})")
 
 
 def run():
